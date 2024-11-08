b5758e2eeaa983f17da4e2d99cc3fa31cc631d0d
wenwenwen555 <wenzhengkun2020@163.com>
2024-11-08 16:57:58 +0800

    模拟实现登陆功能

---
+++ src/main/java/com/kun/springboottest/Controller/UserController.java
@@ -0,0 +1,13 @@
+package com.kun.springboottest.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @RequestMapping("login")
+    public String login(){
+        return "login success!";
+    }
+}
